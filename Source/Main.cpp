4bfbc984b1349381a4037364acb5c223361e2277
Karan semwal <96228962+Karansemwal@users.noreply.github.com>
2022-12-15 16:18:25 +0530

    Code modification

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -7,8 +7,8 @@ char Cell[3][3];
 int main()
 {
 	
-	const char Player1 = 'X';
-	const char Player2 = 'O';
+	const char Player1 = 'O';
+	const char Player2 = 'X';
 
 
 /*	std::cout << " " << Cell[0][0] << " | " << Cell[0][1] << " | " << Cell[0][2] << std::endl;
@@ -17,7 +17,11 @@ int main()
 	std::cout << "--- " << "--- " << "--- "<< std::endl;
 	std::cout << " " << Cell[2][0] << " | " << Cell[2][1] << " | " << Cell[2][2] << std::endl;  */
 
-	Update_TTT();
+	Game_Description();
+	Draw_Board();
+	Play();
+
+	
 
 	std::cin.get();
 }
